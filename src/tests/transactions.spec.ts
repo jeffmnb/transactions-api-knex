e5a5c9f89fadb04d277e2fd3e90e55129157517f
jeffmnb <jefeson.nborda@gmail.com>
2024-11-05 22:32:12 -0300

    test: imploving unit tests

--- src/tests/transactions.spec.ts
+++ src/tests/transactions.spec.ts
@@ -17,29 +17,49 @@ describe("transactions routes", () => {
     await app.close()
   })
 
-  it("should return succes when create a new transaction", async () => {
-    const { statusCode } = await request(app.server)
-      .post("/transactions")
-      .send({
+  describe("creating new transactiion", () => {
+    it("should return succes when create a new transaction", async () => {
+      const { statusCode } = await request(app.server)
+        .post("/transactions")
+        .send({
+          title: "Freela",
+          amount: 1000,
+          type: "credit",
+        })
+      expect(statusCode).toBe(201)
+    })
+
+    it("should return fail when create a new wrong transaction", async () => {
+      const { statusCode } = await request(app.server)
+        .post("/transactions")
+        .send({
+          title: 123,
+          amount: "1000",
+          type: "credit",
+        })
+      expect(statusCode).toBe(400)
+    })
+  })
+
+  describe("get transactiion", () => {
+    it("should return succes when get transactions", async () => {
+      const response = await request(app.server).post("/transactions").send({
         title: "Freela",
         amount: 1000,
         type: "credit",
       })
-    expect(statusCode).toBe(201)
-  })
 
-  it("should return succes when get transactions", async () => {
-    const response = await request(app.server).post("/transactions").send({
-      title: "Freela",
-      amount: 1000,
-      type: "credit",
+      const cookies = response.get("Set-Cookie")
+
+      const { statusCode } = await request(app.server)
+        .get("/transactions")
+        .set("Cookie", cookies!)
+      expect(statusCode).toBe(200)
     })
 
-    const cookies = response.get("Set-Cookie")
-
-    const { statusCode } = await request(app.server)
-      .get("/transactions")
-      .set("Cookie", cookies!)
-    expect(statusCode).toBe(200)
+    it("should return unauthorized when get transactions without cookies", async () => {
+      const { statusCode } = await request(app.server).get("/transactions")
+      expect(statusCode).toBe(401)
+    })
   })
 })
